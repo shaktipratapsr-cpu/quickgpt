d799b9b60360df8c386a5b355f5d475c96311469
shaktipratapsr-cpu <shaktipratapsr@gmail.com>
2026-01-28 15:12:41 +0530

    Fix: Add .jsx extensions back to all imports for Vercel Linux module resolution

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useAppContext } from '../context/AppContext'
+import { useAppContext } from '../context/AppContext.jsx'
 import { assets } from '../assets/assets'
 import moment from 'moment'
 import toast from 'react-hot-toast'
